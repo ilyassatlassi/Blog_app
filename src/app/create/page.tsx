13cfc26b0f5f5abd42b6053be3a81ded199c384c
ilyass atlassi <atlassiilyass@gmail.com>
2024-01-09 18:58:28 +0100

    fix linters

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -9,8 +9,9 @@ import { useMutation } from "@tanstack/react-query"
 import toast from "react-hot-toast"
 import { useRouter } from "next/navigation"
 
-const router = useRouter()
+
 const CreatePage = () => {
+    const router = useRouter()
     const { mutate: createPost, isError: createError, isPending:creatPending , isSuccess: createSucces } = useMutation({
         mutationFn: async (values: z.infer<typeof formSchema>)  => {
             // await new Promise((resolve) => setTimeout(resolve, 2000))
